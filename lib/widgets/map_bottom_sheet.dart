98d537ae421a75df83900e7298b523bd973d4e9e
Daniel Febrian <danielfebrian61@gmail.com>
2022-12-23 16:56:35 +0700

    fixed map typo bug

--- lib/widgets/map_bottom_sheet.dart
+++ lib/widgets/map_bottom_sheet.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:osm_nominatim/osm_nominatim.dart';
 
 class MapBottomSheet extends StatelessWidget {
@@ -17,7 +14,7 @@ class MapBottomSheet extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Container(
+      child: SizedBox(
         height: 200,
         width: size.width,
         child: Column(
@@ -25,17 +22,17 @@ class MapBottomSheet extends StatelessWidget {
           children: [
             Text(
               place.address?["shop"] ?? "Null",
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.w600,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
             Text(
               address,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 14,
               ),
             ),
